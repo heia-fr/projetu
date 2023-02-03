6e1fe1009c6577c538329e85c6f5ade236e0e91c
Damien Goetschi <damien.goetschi@hefr.ch>
2023-02-03 14:23:07 +0100

    Update style

--- projetu/web_template/themes/hugo-book/assets/search.js
+++ projetu/web_template/themes/hugo-book/assets/search.js
@@ -5,7 +5,7 @@
 {{ $searchConfig := i18n "bookSearchConfig" | default "{}" }}
 
 (function () {
-  const searchDataURL = '{{ $searchData.RelPermalink }}';
+  const searchDataURL = '.{{ $searchData.RelPermalink }}';
   const indexConfig = Object.assign({{ $searchConfig }}, {
     doc: {
       id: 'id',
